60abafceba9feb36c16a6549cdeb1861d2638564
Benjamin Schindler <bschindler@informatik.uni-leipzig.de>
2024-03-27 17:24:15 +0100

    reworked on weighting

--- discr_ensemble.py
+++ discr_ensemble.py
@@ -12,15 +12,17 @@ import torchinfo
 
 import json
 import copy
+import warnings
 from typing import Union
 from easydict import EasyDict as edict
 
-from ensemble_utils import reduce_output, split_and_weight, soft_weighting, soft_weighting_autograd
+from ensemble_utils import reduce_output, get_gradient_weighting
 
-'''
+
 from functorch import combine_state_for_ensemble
 from functorch import vmap
 
+'''
 class DiscriminatorGroup(nn.Module):
     """
     A Group of vectorized Discriminators with the exact same architector
@@ -49,8 +51,105 @@ class DiscriminatorEnsemble(nn.Module):
     """
 
     def __init__(self, discr_class: Union[type, list[type]], config: Union[dict, list] = None, multiplier: int = 1,
-                 weighting: str = 'ew', lambda_var=1, *args, **kwargs):
+                 weighting: str = 'ew', lambda_var=1, isStudioGAN=False, *args, **kwargs):
         super().__init__()
+        self.isStudioGAN = isStudioGAN
+
+        # convert discr_class to a list if single type is given:
+        if not isinstance(discr_class, list):
+            discr_class = [discr_class]
+
+        # convert config to a list if only one configuration is given:
+        if not isinstance(config, list):
+            config = [config]
+
+        if config is not None and len(discr_class) != len(config):
+            raise ValueError("Number of given discr_classes must equal the number of configs")
+
+        if weighting not in ["ew", "soft", "rand_uniform", "rand_normal", "rand_bernoulli"]:
+            raise ValueError('Weighting must be in ["ew", "soft", "rand_uniform", "rand_normal", "rand_bernoulli"].')
+
+        self.n_of_groups = len(discr_class)
+        self.group_size = multiplier
+        self.n_of_discr = len(discr_class) * multiplier
+        self.lambda_var = torch.tensor(lambda_var)
+        self.weighting_method = weighting
+        self.weight = get_gradient_weighting(weighting)
+
+        self.discriminators = nn.ModuleList()
+
+        for idx, module_class in enumerate(discr_class):
+
+            for _ in range(multiplier):
+                if self.isStudioGAN: # -> don't use config, but pass arguments directly
+                    self.discriminators.append(module_class(*args, **kwargs))
+                else:
+                    self.discriminators.append(module_class(config[idx], *args, **kwargs))
+
+
+    def forward_single(self, x, idx, *args, **kwargs):
+        return self.discriminators[idx](x, *args, **kwargs)
+
+    def forward(self, x, *args, **kwargs):
+        """Neural Network Forward Propagation with equal loss weighting."""
+        # gradient weighting only in case of an attached generator:
+        gen_attached = not x.is_leaf
+        # split to discriminators:
+        feature_dims = x.dim()
+        x = x.expand([self.n_of_discr] + [-1] * feature_dims)
+
+        # apply gradient weighting layers:
+        if self.weighting_method == "soft" and gen_attached:
+            # allocate tensor for discriminator predicts:
+            discr_out = torch.zeros([x.shape[1], self.n_of_discr], device=x.device)  # Batch X Discr
+            x = self.weight(x, self.lambda_var, discr_out)
+        elif gen_attached:
+            x = self.weight(x)
+
+        # forward through individual discriminators:
+        if self.isStudioGAN:
+            result_dicts = [discr(x[idx],*args, **kwargs) for (idx, discr) in enumerate(self.discriminators)]
+            x = torch.stack([result["adv_output"] for result in result_dicts], dim=-1)
+        else:
+            x = torch.cat([discr(x[idx],*args, **kwargs) for (idx, discr) in enumerate(self.discriminators)], dim=-1)
+
+        # inplace update of discriminator predicts, needed for soft-weighting during Backpropagation:
+        if self.weighting_method == "soft" and gen_attached:
+            # adapt predicts in case of paccing (Pac-GAN):
+            pac_size = discr_out.shape[0] // x.shape[0]
+            unpacked_x = torch.repeat_interleave(x, pac_size, dim=0)
+            discr_out.add_(unpacked_x)
+
+        # evaluation mode -> reduce predictions to single output:
+        if not self.training:
+            x = reduce_output(x)
+
+        # Add additional Information to output when using StudioGAN:
+        if self.isStudioGAN:
+            if self.training:  # evaluation mode -> single output
+                labels = torch.stack([result["label"] for result in result_dicts], dim=-1)
+            else:
+                labels = result_dicts[0]["label"].unsqueeze(-1)
+            if x.shape != labels.shape:
+                warnings.warn("Label shape does not equal output shape")
+            x = {"adv_output": x, "label": labels}
+        return x
+
+
+'''
+class DiscriminatorEnsemble_old(nn.Module):
+    """
+    Discriminator MLP-Discriminator.
+
+    This implements a
+    config-arguments:
+    - discriminators: number of input features
+    """
+
+    def __init__(self, discr_class: Union[type, list[type]], config: Union[dict, list] = None, multiplier: int = 1,
+                 weighting: str = 'ew', lambda_var=1, isStudioGAN=False, *args, **kwargs):
+        super().__init__()
+        self.isStudioGAN = isStudioGAN
 
         # convert discr_class to a list if single type is given:
         if not isinstance(discr_class, list):
@@ -76,40 +175,38 @@ class DiscriminatorEnsemble(nn.Module):
         for idx, module_class in enumerate(discr_class):
 
             for _ in range(multiplier):
-                if config[idx] is None: #StudioGAN
+                if self.isStudioGAN: # -> don't use config, but pass arguments directly
                     self.discriminators.append(module_class(*args, **kwargs))
                 else:
                     self.discriminators.append(module_class(config[idx], *args, **kwargs))
-
-            #code for vectorized group
-            #if multiplier>1:
-            #    group = DiscriminatorGroup(discr_class=module_class, multiplier=multiplier,  config=config[idx], *args, **kwargs)
-            #    self.discriminators.append(group)
-            #else:
-            #    self.discriminators.append(module_class(config[idx], *args, **kwargs))
 
         self.split_and_weight = lambda x: split_and_weight.apply(x, self.n_of_discr, weighting)
         self.apply_soft_weighting = weighting == "soft"
 
 
-    def forward_single(self, x, idx):
-        return self.discriminators[idx](x)
+    def forward_single(self, x, idx, *args, **kwargs):
+        return self.discriminators[idx](x, *args, **kwargs)
 
     def forward(self, x, *args, **kwargs):
         """Neural Network Forward Propagation with equal loss weighting."""
         x = self.split_and_weight(x)
         if self.apply_soft_weighting:
-            discr_out = torch.zeros([x.shape[1], self.n_of_discr])  # Batch X Discr
+            # allocate tensor for discriminator predicts:
+            discr_out = torch.zeros([x.shape[1], self.n_of_discr], device=x.device)  # Batch X Discr
             x = soft_weighting_autograd(x, self.lambda_var, discr_out)
-        if args is not None: #StudioGAN
-            result_dicts = [discr(x[idx],*args, **kwargs) for (idx, discr) in enumerate(self.discriminators)]
-            x = torch.concat([result["adv_output"] for result in result_dicts], dim=-1)
-        else:
-            x = torch.concat([discr(x[idx],*args, **kwargs) for (idx, discr) in enumerate(self.discriminators)], dim=-1)
 
+        #forward through individual discriminators:
+        if self.isStudioGAN:
+            result_dicts = [discr(x[idx],*args, **kwargs) for (idx, discr) in enumerate(self.discriminators)]
+            x = torch.stack([result["adv_output"] for result in result_dicts], dim=-1)
+        else:
+            x = torch.cat([discr(x[idx],*args, **kwargs) for (idx, discr) in enumerate(self.discriminators)], dim=-1)
+
+        # take to
         if self.apply_soft_weighting:
             # manipulate discr_out Tensor that is used for soft-weighting during Backpropagation:
             # fix for pacsize:
+            #ToDo fix for StudioGAN:
             pac_size = discr_out.shape[0] // x.shape[0]
             unpacced_x = torch.repeat_interleave(x, pac_size, dim=0)
             discr_out.add_(unpacced_x)
@@ -117,10 +214,13 @@ class DiscriminatorEnsemble(nn.Module):
         if not self.training:  # evaluation mode -> single output
             x = reduce_output(x)
 
-        if args is not None: #StudioGAN
-            labels = torch.concat([result["label"] for result in result_dicts], dim=-1)
-            #Debug_
-            if x.shape[0] != labels.shape[0]:
+        if self.isStudioGAN:
+            if self.training:  # evaluation mode -> single output
+                labels = torch.stack([result["label"] for result in result_dicts], dim=-1)
+            else:
+                labels = result_dicts[0]["label"].unsqueeze(-1)
+            if x.shape != labels.shape:
                 warnings.warn("Label shape does not equal output shape")
             x = {"adv_output": x, "label": labels}
         return x
+'''
--- ensemble_utils.py
+++ ensemble_utils.py
@@ -26,6 +26,114 @@ class reduce_output_function(torch.autograd.Function):
         discr_outputs, = ctx.saved_tensors
         return grad_output.repeat(discr_outputs.shape[0])
 
+class split_to_discriminators(torch.autograd.Function):
+    """expand sample as an input to multiple discriminators, on backward, aggregate """
+
+    @staticmethod
+    def forward(ctx, tensor, n_of_discr):
+        """
+        Just forward the input.
+        """
+        ctx.gen_attached = not tensor.is_leaf,
+        ctx.n_of_discr =  n_of_discr
+        return tensor.expand([n_of_discr] + [-1]*tensor.dim())
+
+    @staticmethod
+    def backward(ctx, tensor_grad):
+        """
+        Random weight the gradients according to normal distribuation.
+        """
+        return tensor_grad.sum(dim=0), None
+
+class weight_rand_uniform(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, tensor):
+        return tensor
+
+    @staticmethod
+    def backward(ctx, tensor_grad):
+        n_of_discr = tensor_grad.shape[0]
+        w = F.softmax(torch.rand(n_of_discr, device=tensor_grad.device), dim=0)
+        view_shape = [-1] + [1] * (tensor_grad.dim() - 1)
+        tensor_grad = tensor_grad * w.view(view_shape)
+        return tensor_grad
+
+class weight_rand_normal(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, tensor):
+        return tensor
+
+    @staticmethod
+    def backward(ctx, tensor_grad):
+        n_of_discr = tensor_grad.shape[0]
+        w = F.softmax(torch.randn(n_of_discr, device=tensor_grad.device), dim=0)
+        view_shape = [-1] + [1] * (tensor_grad.dim() - 1)
+        tensor_grad = tensor_grad * w.view(view_shape)
+        return tensor_grad
+
+class weight_rand_bernoulli(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, tensor):
+        return tensor
+
+    @staticmethod
+    def backward(ctx, tensor_grad):
+        n_of_discr = tensor_grad.shape[0]
+        w = torch.bernoulli(torch.tensor([0.5], device=tensor_grad.device).repeat(n_of_discr))
+        if w.sum() == 0.:
+            w[torch.randint(high=n_of_discr, size=[1])] = 1.
+        else:
+            w.div(w.sum())
+        view_shape = [-1] + [1] * (tensor_grad.dim() - 1)
+        tensor_grad = tensor_grad * w.view(view_shape)
+        return tensor_grad
+
+class weight_by_predicts(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, tensor, lambda_var, discr_outputs):
+        """
+        Forward the
+        """
+        ctx.lambda_var = lambda_var
+        ctx.discr_outputs = discr_outputs
+        return tensor
+
+    @staticmethod
+    def backward(ctx, tensor_grad):
+        if ctx.discr_outputs._version != 1:
+            raise RuntimeError("Tensor of discr_output have to be changed exactly one (at the end of forward path)")
+        feature_dims = tensor_grad.dim() - 2
+        no_of_discr = ctx.discr_outputs.shape[1]
+        w = F.softmax(ctx.discr_outputs * ctx.lambda_var, dim=1)
+        tensor_grad = tensor_grad * w.movedim(0, -1).view([no_of_discr, -1] + [1] * feature_dims)
+        return tensor_grad, None, None
+
+
+def reduce_output(x):
+    return reduce_output_function.apply(x)
+
+def get_gradient_weighting(weighting: str):
+    if weighting not in ["ew", "soft", "rand_uniform", "rand_normal", "rand_bernoulli"]:
+        raise ValueError('Weighting must be in ["ew", "soft", "rand_uniform", "rand_normal", "rand_bernoulli"].')
+    if weighting == "ew":
+        return lambda x: x
+    if weighting == "rand_uniform":
+        return lambda x: weight_rand_uniform.apply(x)
+    if weighting == "rand_normal":
+        return lambda x: weight_rand_normal.apply(x)
+    if weighting == "rand_bernoulli":
+        return lambda x: weight_rand_bernoulli.apply(x)
+    if weighting == "soft":
+        return lambda x, lambda_var, discr_outputs: weight_by_predicts.apply(x, lambda_var, discr_outputs)
+
+
+'''
+def soft_weighting_autograd(*args):
+    return soft_weighting_function.apply(*args)
+    
+
 class split_and_weight(torch.autograd.Function):
     """expand sample as an input to multiple discriminators,
     do a weighting on backward if the sample is attached to a generator (no leaf)"""
@@ -47,15 +155,15 @@ class split_and_weight(torch.autograd.Function):
         """
         if ctx.gen_attached:
             if ctx.weighting == "rand_uniform":
-                w = F.softmax(torch.rand(ctx.n_of_discr), dim=0)
+                w = F.softmax(torch.rand(ctx.n_of_discr, device=tensor_grad.device), dim=0)
                 view_shape = [-1] + [1] * (tensor_grad.dim()-1)
                 tensor_grad = tensor_grad *  w.view(view_shape)
             if ctx.weighting == "rand_normal":
-                w = F.softmax(torch.randn(ctx.n_of_discr), dim=0)
+                w = F.softmax(torch.randn(ctx.n_of_discr, device=tensor_grad.device), dim=0)
                 view_shape = [-1] + [1] * (tensor_grad.dim()-1)
                 tensor_grad = tensor_grad *  w.view(view_shape)
             if ctx.weighting == "rand_bernoulli":
-                w = torch.bernoulli(torch.tensor([0.5]).repeat(ctx.n_of_discr))
+                w = torch.bernoulli(torch.tensor([0.5], device=tensor_grad.device).repeat(ctx.n_of_discr))
                 if w.sum() == 0.:
                     w[torch.randint(high=ctx.n_of_discr, size=[1])] = 1.
                 else:
@@ -65,51 +173,6 @@ class split_and_weight(torch.autograd.Function):
         return tensor_grad.sum(dim=0), None, None
 
 
-class split_and_soft_weight(torch.autograd.Function):
-    """expand sample as an input to multiple discriminators,
-    do a weighting on backward if the sample is attached to a generator (no leaf)"""
-
-    @staticmethod
-    def forward(ctx, tensor, n_of_discr, weighting):
-        """
-        Just forward the input.
-        """
-        ctx.gen_attached = not tensor.is_leaf,
-        ctx.n_of_discr =  n_of_discr
-        ctx.weighting = weighting
-        return tensor.expand([n_of_discr] + [-1]*tensor.dim())
-
-    @staticmethod
-    def backward(ctx, tensor_grad):
-        """
-        Random weight the gradients according to normal distribuation.
-        """
-        if ctx.gen_attached:
-            if ctx.weighting == "rand_uniform":
-                w = F.softmax(torch.rand(ctx.n_of_discr), dim=0)
-                view_shape = [-1] + [1] * (tensor_grad.dim()-1)
-                tensor_grad = tensor_grad *  w.view(view_shape)
-            if ctx.weighting == "rand_normal":
-                w = F.softmax(torch.randn(ctx.n_of_discr), dim=0)
-                view_shape = [-1] + [1] * (tensor_grad.dim()-1)
-                tensor_grad = tensor_grad *  w.view(view_shape)
-            if ctx.weighting == "rand_bernoulli":
-                w = torch.bernoulli(torch.tensor([0.5]).repeat(ctx.n_of_discr))
-                if w.sum() == 0.:
-                    w[torch.randint(high=ctx.n_of_discr, size=[1])] = 1.
-                else:
-                    w.div(w.sum())
-                view_shape = [-1] + [1] * (tensor_grad.dim() - 1)
-                tensor_grad = tensor_grad * w.view(view_shape)
-        return tensor_grad.sum(dim=0), None, None
-
-
-def soft_weighting(grad, predictions, lambda_var):
-    "weighting according to discriminator predictions, GMAN paper"
-    w = F.softmax(predictions * lambda_var, dim=0)
-    view_shape = [-1] + [1] * (grad.dim() - 1)
-    grad = grad * w.view(view_shape)
-    return grad
 
 
 
@@ -135,14 +198,13 @@ class soft_weighting_function(torch.autograd.Function):
         if ctx.gen_attached:
             if ctx.discr_outputs._version !=1:
                 raise RuntimeError("Tensor of discr_output have to be changed exactly one (at the end of forward path)")
+            feature_dims = tensor_grad.dim() - 2
+            no_of_discr = ctx.discr_outputs.shape[1]
             w = F.softmax(ctx.discr_outputs * ctx.lambda_var, dim=1)
-            tensor_grad = tensor_grad * w.movedim(0,-1).view(ctx.discr_outputs.shape[1],-1,1)
+            tensor_grad = tensor_grad * w.movedim(0,-1).view([no_of_discr,-1] + [1] * feature_dims)
         return tensor_grad, None, None
+        
+'''
 
-def reduce_output(x):
-    return reduce_output_function.apply(x)
-
-def soft_weighting_autograd(*args):
-    return soft_weighting_function.apply(*args)
 
 
